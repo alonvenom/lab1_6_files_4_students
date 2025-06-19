bb7570678010f0d513c57e525433db99da8c8a2f
al <156125857+alonvenom@users.noreply.github.com>
2025-06-19 19:40:03 +0300

    Update test_4_object_detection_image_creation.py

--- test_4_object_detection_image_creation.py
+++ test_4_object_detection_image_creation.py
@@ -1,13 +1,14 @@
 import pickle
 import cv2
-import gdown
-# import matplotlib.pyplot as plt
+import urllib.request
 import numpy as np
 from main import run_main_anomaly_loop
 import pytest
 import os
 import params_links
 import glob
+
+
 @pytest.mark.order(4)
 @pytest.mark.parametrize("input_video_name, input_video_link, jpg_file_name, size",
                          [('test1.mp4',
@@ -18,10 +19,18 @@ import glob
 def test_check_created_jpg_file(input_video_name, input_video_link, jpg_file_name, size):
     current_directory = os.getcwd()
     input_video_name = os.path.join(current_directory, input_video_name)
-    gdown.download(input_video_link, input_video_name)
-    # run_main_anomaly_loop(input_video_name) # no need to run this!
-    # assert True if 1.0_*.jpg file exists in /bbox_images directory
+
+    if not os.path.exists(input_video_name):
+        urllib.request.urlretrieve(input_video_link, input_video_name)
+
+    # אין צורך להריץ שוב אם הקבצים כבר נוצרו
+    # run_main_anomaly_loop(input_video_name)
+
+
     files = glob.glob(f'{current_directory}/bbox_images/{jpg_file_name}')
-    assert files, f'{jpg_file_name} does not exist'
+    assert files, f'{jpg_file_name} does not exist in bbox_images directory'
+
+    if len(files) < size:
+        assert False, f'Expected at least {size} files, found {len(files)}'
 
     pass
