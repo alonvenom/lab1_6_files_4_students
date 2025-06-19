b4392a97cf2c866d3c6b2abe099c99ed517a0ae7
al <156125857+alonvenom@users.noreply.github.com>
2025-06-19 20:07:12 +0300

    Update test_10_routine_map_created.py

--- test_10_routine_map_created.py
+++ test_10_routine_map_created.py
@@ -1,6 +1,6 @@
 import pickle
 import cv2
-import gdown
+import urllib.request
 # import matplotlib.pyplot as plt
 import numpy as np
 from main import run_main_routine_loop
@@ -19,8 +19,8 @@ def test_save_heat_map_as_pkl(input_video_name, input_video_link, heatmap_gt_fil
                               max_error):
     current_directory = os.getcwd()
     video_file = os.path.join(current_directory, input_video_name)
-    if not os.path.exists(input_video_name):
-        gdown.download(input_video_link, input_video_name)
+    if not os.path.exists(video_file):
+       urllib.request.urlretrieve(input_video_link, video_file)
     #run_main_routine_loop(video_file)
     pkl_file_path = os.path.join(os.getcwd(), 'routine_map.pkl')
     assert os.path.exists(pkl_file_path)
